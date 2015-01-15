dee3b6fd8bf3d648d635e8bd2c3e1db0ad7e51ca
Eric Kennedy <ekeric13@gmail.com>
2015-01-15 14:28:32 -0800

    Refactored config file to use mongo

--- app/config.js
+++ app/config.js
@@ -1,53 +1,18 @@
-var Bookshelf = require('bookshelf');
-var path = require('path');
-var host = process.env.HOST || '127.0.0.1';
-if (process.NODE_ENV === "production"){
-  var database = "shortlydb";
-  var filename = path.join(__dirname, '../db/'+database+'.sqlite')
-} else {
-  var database = "shortlydevdb";
-  var filename = path.join(__dirname, '../db/'+database+'.sqlite')
+var mongoose = require('mongoose');
+
+var database = 'mongodb://127.0.0.1:27017/shortly';
+
+if (process.env.NODE_ENV === 'production') {
+   database = 'mongodb://MongoLab-x:TV00Bq0IO7nKsCuRSn9AnOsjpaXfxjeZyYrzhZ3VgCs-@ds052827.mongolab.com:52827/MongoLab-x';
 }
 
-var db = Bookshelf.initialize({
-  client: 'sqlite3',
-  connection: {
-    host: host,
-    user: 'your_database_user',
-    password: 'password',
-    database: database,
-    charset: 'utf8',
-    filename: filename
-  }
-});
+mongoose.connect(database);
 
-db.knex.schema.hasTable('urls').then(function(exists) {
-  if (!exists) {
-    db.knex.schema.createTable('urls', function (link) {
-      link.increments('id').primary();
-      link.string('url', 255);
-      link.string('base_url', 255);
-      link.string('code', 100);
-      link.string('title', 255);
-      link.integer('visits');
-      link.timestamps();
-    }).then(function (table) {
-      console.log('Created Table', table);
-    });
-  }
-});
+var db = mongoose.connection;
 
-db.knex.schema.hasTable('users').then(function(exists) {
-  if (!exists) {
-    db.knex.schema.createTable('users', function (user) {
-      user.increments('id').primary();
-      user.string('username', 100).unique();
-      user.string('password', 100);
-      user.timestamps();
-    }).then(function (table) {
-      console.log('Created Table', table);
-    });
-  }
+db.on('error', console.error.bind(console, 'connection error:'));
+db.on('open', function() {
+  console.log('mongo connected');
 });
 
 module.exports = db;
