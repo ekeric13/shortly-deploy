5cee16807008faf2effdd90be628aeabccd3ddbe
Eric Kennedy <ekeric13@gmail.com>
2015-01-15 14:30:05 -0800

    Refactored users model to use mongo

--- app/models/user.js
+++ app/models/user.js
@@ -1,25 +1,27 @@
-var db = require('../config');
+var mongoose = require('mongoose');
 var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
 
-var User = db.Model.extend({
-  tableName: 'users',
-  hasTimestamps: true,
-  initialize: function(){
-    this.on('creating', this.hashPassword);
-  },
-  comparePassword: function(attemptedPassword, callback) {
-    bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
-      callback(isMatch);
-    });
-  },
-  hashPassword: function(){
-    var cipher = Promise.promisify(bcrypt.hash);
-    return cipher(this.get('password'), null, null).bind(this)
-      .then(function(hash) {
-        this.set('password', hash);
-      });
-  }
+var userSchema = new mongoose.Schema({
+  username: { type: String, unique: true },
+  password: { type: String }
+});
+
+var User = mongoose.model('User', userSchema);
+
+User.prototype.comparePassword = function(attemptedPassword, callback) {
+  bcrypt.compare(attemptedPassword, this.password, function(err, isMatch) {
+    callback(err, isMatch);
+  });
+};
+
+
+userSchema.pre('save', function(next) {
+  var cipher = Promise.promisify(bcrypt.hash);
+  return cipher(this.password, null, null).bind(this).then(function(hash) {
+    this.password = hash;
+    next();
+  });
 });
 
 module.exports = User;
